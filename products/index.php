f99e4988c39eb1558aeddefde5ebf9b20051888d
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-10-14 07:42:52 +0000

    Null fix

--- products/index.php
+++ products/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,31 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <script>
-     
+        $(document).ready(function () {
+            var apiUrl = "http://localhost/APIs/API%20Test/ajax/product/";
+
+            function read() {
+                $.ajax({
+                    type: "GET",
+                    url: apiUrl + "read",
+                    dataType: "json",
+                    success: function (data) {
+                        console.log("Data retrieved from the API:", data);
+                    },
+                    error: function (xhr, status, error) {
+                        console.log("Error status:", status);
+                        console.log("Error message:", error);
+                        console.log("Response:", xhr.responseText);
+                    }
+                });
+            }
+
+            read();
+        });
     </script>
-
-
 </head>
+
 <body>
-    
 </body>
+
 </html>
